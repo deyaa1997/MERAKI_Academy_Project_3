5c9cc28cba2982acf61c8d2a95cd1ab34e578a37
deyaa1997 <deyah.mosa@gmail.com>
2021-05-29 13:42:57 +0300

    Use effect state in dashboard componenet

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import axios from "axios"
 export default function Dashboard({token}) {
     const [articles,setArticles] = useState([])
+    useEffect(() => {
+        if (token){
+        axios.get("http://localhost:5000/articles").then( function(res){
+            setArticles(res.data)
+            
+        })
+    }}
+
+    , []);
     const getArticle = ()=>{
         if (token){
-        axios.get("http://localhost:5000/articles").then(async function(res){
+        axios.get("http://localhost:5000/articles").then( function(res){
             setArticles(res.data)
             console.log(res)
             
